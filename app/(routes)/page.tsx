46682c4947abee73fb68d5d970fc390c4a371dd5
Daniel Okello <43291086+okellodaniel@users.noreply.github.com>
2023-12-11 22:35:44 +0300

    Created the billboard type and image

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,8 +1,18 @@
+import getBillboards from "@/actions/get-billboards";
+import Billboard from "@/components/billboard";
+import Container from "@/components/ui/container";
 
-export default function HomePage() {
+export const revalidate = 0;
+
+export default async function HomePage() {
+
+  const billboard = await getBillboards("4bf910b0-9cc1-49da-972e-2a99c7ce790c");
+
   return (
-    <>
-      Home Page
-    </>
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+      </div>
+    </Container>
   );
 }
